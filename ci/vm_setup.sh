d13e53410ed2afdf5cd6292e11705655fd151c6f
Maksymilian Graczyk <maksymilian.graczyk@cern.ch>
2024-01-15 12:21:29 +0100

    Replace Gentoo auto-update with manual update prompt in vm_setup.sh

--- ci/vm_setup.sh
+++ ci/vm_setup.sh
@@ -5,6 +5,6 @@ last_update=$(date --date="$(cat $(portageq get_repo_path / gentoo)/metadata/tim
 current_time=$(date +%s)
 
 if [[ $((current_time-last_update)) -ge 604800 ]]; then
-    sudo emerge --sync
-    sudo emerge --verbose --update --deep --newuse @world
+    >&2 echo "Gentoo is too old, please update it first."
+    exit 1
 fi
