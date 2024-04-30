0a983bd79f89a04526a8a7d3345bded8f8485dd9
Maksymilian Graczyk <maksymilian.graczyk@cern.ch>
2024-04-30 11:46:03 +0200

    Add CMake options to build.sh and simplify rebuilding adaptiveperf-server

--- build.sh
+++ build.sh
@@ -27,17 +27,20 @@ trap "error" ERR
 echo_main "Checking build dir..."
 if [[ -d build ]]; then
     echo_sub "Non-empty build dir detected! Please run clean.sh first." 1
-    echo_sub "If you want to rebuild the server, go to the build dir and run \"make && mv adaptiveperf-server ..\" to get the adaptiveperf-server binary."
+    echo_sub "If you want to rebuild the server, go to the build dir and run \"./make.sh\" to get the adaptiveperf-server binary."
     echo_sub "If you want to rebuild the bash frontend, run \"make\" to get the adaptiveperf shell script."
     exit 1
 else
     mkdir build
+    echo "#!/bin/bash" > build/make.sh
+    echo "cmake --build . && mv adaptiveperf-server ../" >> build/make.sh
+    chmod +x build/make.sh
 fi
 
 echo_main "Building adaptiveperf-server..."
 cd build
-cmake ../src/server
-make
+cmake ../src/server $@
+cmake --build .
 mv adaptiveperf-server ../
 
 echo_main "Building adaptiveperf..."
