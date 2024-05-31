036b4ffcb6412764536c0a057cbb795e1d868569
Maksymilian Graczyk <maksymilian.graczyk@cern.ch>
2024-05-31 13:41:23 +0200

    Fix subclient.cpp bugs detected by automated testing

--- src/server/subclient.cpp
+++ src/server/subclient.cpp
@@ -93,7 +93,7 @@ namespace aperf {
                                                                     acceptor,
                                                                     profiled_filename,
                                                                     buf_size) {
-
+    this->json_result = nlohmann::json::object();
   }
 
   void StdSubclient::process() {
@@ -111,9 +111,6 @@ namespace aperf {
     };
 
     try {
-      std::shared_ptr<Connection> connection = this->acceptor->accept(this->buf_size);
-      this->context.notify();
-
       std::unordered_set<std::string> messages_received;
       std::unordered_map<std::string, std::vector<std::string> > tid_dict;
       std::unordered_map<
@@ -122,164 +119,181 @@ namespace aperf {
           std::string,
           struct sample_result > > subprocesses;
       std::unordered_map<std::string, std::string> combo_dict;
-      std::unordered_map<std::string, unsigned long long> time_dict, exit_time_dict;
-      std::unordered_map<std::string, std::string> name_dict;
+      std::unordered_map<std::string, unsigned long long> exit_time_dict;
+      std::unordered_map<std::string, std::vector<std::pair<std::string, unsigned long long> > > name_time_dict;
       std::unordered_map<std::string, std::string> tree;
+      std::unordered_map<std::string, unsigned long long> first_sample_time_dict;
 
       std::string extra_event_name = "";
+      bool first_event_received = false;
       std::vector<std::pair<unsigned long long, std::string> > added_list;
 
       std::unordered_map<std::string, std::string> last_clone_flags;
 
-      while (true) {
-        std::string line = connection->read();
+      {
+        std::shared_ptr<Connection> connection = this->acceptor->accept(this->buf_size);
+        this->context.notify();
 
-        if (line == "<STOP>") {
-          break;
-        }
+        while (true) {
+          std::string line = connection->read();
 
-        nlohmann::json arr;
+          if (line == "<STOP>") {
+            break;
+          }
 
-        try {
-          arr = nlohmann::json::parse(line);
-        } catch (...) {
-          std::cerr << "Could not parse the recently-received line to JSON, ignoring." << std::endl;
-          continue;
-        }
-
-        if (!arr.is_array() || arr.empty()) {
-          std::cerr << "The recently-received JSON is not a non-empty array, ignoring." << std::endl;
-          continue;
-        }
-
-        messages_received.insert(arr[0]);
-
-        if (arr[0] == "<SYSCALL>") {
-          std::string ret_value;
-          std::vector<std::string> callchain;
+          nlohmann::json arr;
 
           try {
-            ret_value = arr[1];
-            callchain = arr[2];
+            arr = nlohmann::json::parse(line);
           } catch (...) {
-            std::cerr << "The recently-received syscall JSON is invalid, ignoring." << std::endl;
+            std::cerr << "Could not parse the recently-received line to JSON, ignoring." << std::endl;
             continue;
           }
 
-          tid_dict[ret_value] = callchain;
-        } else if (arr[0] == "<SYSCALL_TREE>") {
-          std::string syscall_type, comm_name, pid, tid, ret_value;
-          unsigned long long time;
-
-          try {
-            syscall_type = arr[1];
-            comm_name = arr[2];
-            pid = arr[3];
-            tid = arr[4];
-            time = arr[5];
-            ret_value = arr[6];
-          } catch (...) {
-            std::cerr << "The recently-received syscall tree JSON is invalid, ignoring." << std::endl;
+          if (!arr.is_array() || arr.empty()) {
+            std::cerr << "The recently-received JSON is not a non-empty array, ignoring." << std::endl;
             continue;
           }
 
-          std::string pid_tid = pid + "/" + tid;
+          messages_received.insert(arr[0]);
 
-          if (syscall_type == "new_proc") {
+          if (arr[0] == "<SYSCALL>") {
+            std::string ret_value;
+            std::vector<std::string> callchain;
+
+            try {
+              ret_value = arr[1];
+              callchain = arr[2];
+            } catch (...) {
+              std::cerr << "The recently-received syscall JSON is invalid, ignoring." << std::endl;
+              continue;
+            }
+
+            tid_dict[ret_value] = callchain;
+          } else if (arr[0] == "<SYSCALL_TREE>") {
+            std::string syscall_type, comm_name, pid, tid, ret_value;
+            unsigned long long time;
+
+            try {
+              syscall_type = arr[1];
+              comm_name = arr[2];
+              pid = arr[3];
+              tid = arr[4];
+              time = arr[5];
+              ret_value = arr[6];
+            } catch (...) {
+              std::cerr << "The recently-received syscall tree JSON is invalid, ignoring." << std::endl;
+              continue;
+            }
+
+            std::string pid_tid = pid + "/" + tid;
+            bool added_to_name_time_dict = false;
+
             if (tree.find(tid) == tree.end()) {
               tree[tid] = "";
               added_list.push_back(std::make_pair(time, tid));
 
-              combo_dict[tid] = pid + "/" + tid;
-
-              if (name_dict.find(tid) == name_dict.end()) {
-                name_dict[tid] = comm_name;
-              }
+              name_time_dict[tid].push_back(std::make_pair(comm_name, time));
+              added_to_name_time_dict = true;
             }
 
-            if (tree.find(ret_value) == tree.end()) {
-              added_list.push_back(std::make_pair(time, ret_value));
-            }
-
-            tree[ret_value] = tid;
-
-            if (time_dict.find(ret_value) == time_dict.end()) {
-              time_dict[ret_value] = time;
-            }
-
-            if (name_dict.find(ret_value) == name_dict.end()) {
-              name_dict[ret_value] = comm_name;
-            }
-          } else if (syscall_type == "execve") {
-            time_dict[tid] = time;
-            name_dict[tid] = comm_name;
-          } else if (syscall_type == "exit") {
-            exit_time_dict[tid] = time;
             combo_dict[tid] = pid + "/" + tid;
-          }
-        } else if (arr[0] == "<SAMPLE>") {
-          std::string event_type, pid, tid;
-          unsigned long long timestamp, period;
-          std::vector<std::string> callchain;
-          try {
-            event_type = arr[1];
-            pid = arr[2];
-            tid = arr[3];
-            timestamp = arr[4];
-            period = arr[5];
-            callchain = arr[6];
-          } catch (...) {
-            std::cerr << "The recently received sample JSON is invalid, ignoring." << std::endl;
-            continue;
-          }
 
-          if (event_type == "offcpu-time" || event_type == "task-clock") {
-            extra_event_name = "";
-          } else {
-            extra_event_name = event_type;
-          }
+            if (syscall_type == "new_proc") {
+              if (tree.find(ret_value) == tree.end()) {
+                added_list.push_back(std::make_pair(time, ret_value));
+              }
 
-          if (subprocesses.find(pid) == subprocesses.end()) {
-            std::unordered_map<std::string, struct sample_result > new_map;
-            subprocesses[pid] = new_map;
-          }
+              tree[ret_value] = tid;
+              combo_dict[ret_value] = "?/" + ret_value;
+              name_time_dict[ret_value].push_back(std::make_pair(comm_name, time));
+            } else if (syscall_type == "execve" && !added_to_name_time_dict) {
+              name_time_dict[tid].push_back(std::make_pair(comm_name, time));
+            } else if (syscall_type == "exit") {
+              exit_time_dict[tid] = time;
+            }
+          } else if (arr[0] == "<SAMPLE>") {
+            std::string event_type, pid, tid;
+            unsigned long long timestamp, period;
+            std::vector<std::string> callchain;
+            try {
+              event_type = arr[1];
+              pid = arr[2];
+              tid = arr[3];
+              timestamp = arr[4];
+              period = arr[5];
+              callchain = arr[6];
+            } catch (...) {
+              std::cerr << "The recently received sample JSON is invalid, ignoring." << std::endl;
+              continue;
+            }
 
-          if (subprocesses[pid].find(tid) == subprocesses[pid].end()) {
-            subprocesses[pid][tid] = {};
+            if (!first_event_received) {
+              first_event_received = true;
+
+              if (event_type == "offcpu-time" || event_type == "task-clock") {
+                extra_event_name = "";
+              } else {
+                extra_event_name = event_type;
+              }
+            } else if ((extra_event_name != "" && event_type != extra_event_name) ||
+                       (extra_event_name == "" && event_type != "offcpu-time" && event_type != "task-clock")) {
+              std::cerr << "The recently received sample JSON is of different event type than expected ";
+              std::cerr << "(received: " << event_type << ", expected: ";
+              std::cerr << (extra_event_name == "" ? "task-clock or offcpu-time" : extra_event_name);
+              std::cerr << "), ignoring." << std::endl;
+              continue;
+            }
+
+            if (first_sample_time_dict.find(pid + "_" + tid) ==
+                first_sample_time_dict.end()) {
+              first_sample_time_dict[pid + "_" + tid] = timestamp;
+            }
+
+            if (subprocesses.find(pid) == subprocesses.end()) {
+              std::unordered_map<std::string, struct sample_result > new_map;
+              subprocesses[pid] = new_map;
+            }
+
+            if (subprocesses[pid].find(tid) == subprocesses[pid].end()) {
+              subprocesses[pid][tid] = {};
+              struct sample_result &res = subprocesses[pid][tid];
+
+              res.output["name"] = "all";
+              res.output["value"] = 0;
+              res.output["children"] = nlohmann::json::array();
+              // Will switch to false as soon as on-CPU activity is encountered
+              res.output["cold"] = true;
+
+              res.output_time_ordered["name"] = "all";
+              res.output_time_ordered["value"] = 0;
+              res.output_time_ordered["children"] = nlohmann::json::array();
+              // Will switch to false as soon as on-CPU activity is encountered
+              res.output_time_ordered["cold"] = true;
+            }
+
             struct sample_result &res = subprocesses[pid][tid];
 
-            res.output["name"] = "all";
-            res.output["value"] = 0;
-            res.output["children"] = nlohmann::json::array();
+            if (event_type == "offcpu-time") {
+              if (callchain.size() <= 1) {
+                callchain.push_back("(just thread/process)");
+              }
 
-            res.output_time_ordered["name"] = "all";
-            res.output_time_ordered["value"] = 0;
-            res.output_time_ordered["children"] = nlohmann::json::array();
-          }
-
-          struct sample_result &res = subprocesses[pid][tid];
-
-          if (event_type == "offcpu-time") {
-            if (callchain.size() <= 1) {
-              callchain.push_back("(just thread/process)");
+              struct offcpu_region reg;
+              reg.timestamp = timestamp - period;
+              reg.period = period;
+              res.offcpu_regions.push_back(reg);
             }
 
-            struct offcpu_region reg;
-            reg.timestamp = timestamp - period;
-            reg.period = period;
-            res.offcpu_regions.push_back(reg);
+            recurse(res.output, callchain, 0, period, false,
+                    event_type == "offcpu-time");
+            recurse(res.output_time_ordered, callchain, 0, period,
+                    true, event_type == "offcpu-time");
+
+            res.total_period += period;
           }
-
-          recurse(res.output, callchain, 0, period, false,
-                  event_type == "offcpu-time");
-          recurse(res.output_time_ordered, callchain, 0, period,
-                  true, event_type == "offcpu-time");
-
-          res.total_period += period;
         }
       }
-
-      connection->close();
 
       std::sort(added_list.begin(), added_list.end(),
                 [] (auto &a, auto &b) { return a.first < b.first; });
@@ -299,20 +313,29 @@ namespace aperf {
           this->json_result[msg_key] = nlohmann::json::array();
           this->json_result[msg_key].push_back(start_time);
           this->json_result[msg_key].push_back(nlohmann::json::array());
+          this->json_result[msg_key].push_back(nlohmann::json::object());
 
           nlohmann::json &result_list = this->json_result[msg_key][1];
+          nlohmann::json &result_map = this->json_result[msg_key][2];
           std::unordered_set<std::string> added_identifiers;
           bool profile_start = false;
 
           for (int i = 0; i < added_list.size(); i++) {
             std::string k = added_list[i].second;
             std::string p = tree[k];
+            int index = -1;
 
             if (!profile_start) {
-              if (name_dict[k] == this->profiled_filename.substr(0, 15)) {
+              for (int i = 0; i < name_time_dict[k].size(); i++) {
+                if (name_time_dict[k][i].first == this->profiled_filename.substr(0, 15)) {
+                  index = i;
+                  break;
+                }
+              }
+
+              if (index != -1) {
                 profile_start = true;
-                start_time = time_dict[k];
-                this->json_result[msg_key][0] = start_time;
+                start_time = name_time_dict[k][index].second;
                 p = "";
               } else {
                 if (p.empty()) {
@@ -330,14 +353,35 @@ namespace aperf {
             added_identifiers.insert(k);
 
             nlohmann::json elem;
-            elem["identifier"] = k;
             elem["tag"] = nlohmann::json::array();
-            elem["tag"][0] = name_dict[k];
+
+            for (int i = 0; i < name_time_dict[k].size(); i++) {
+              if (name_time_dict[k][i].first == this->profiled_filename.substr(0, 15) &&
+                  name_time_dict[k][i].second < start_time) {
+                start_time = name_time_dict[k][i].second;
+              }
+            }
+
+            int dominant_name_index = 0;
+            int dominant_name_time = 0;
+            for (int i = 1; i < name_time_dict[k].size(); i++) {
+              if (name_time_dict[k][i].second - name_time_dict[k][i - 1].second > dominant_name_time) {
+                dominant_name_index = i - 1;
+                dominant_name_time = name_time_dict[k][i].second - name_time_dict[k][i - 1].second;
+              }
+            }
+
+            if (exit_time_dict.find(k) == exit_time_dict.end() ||
+                exit_time_dict[k] - name_time_dict[k][name_time_dict[k].size() - 1].second > dominant_name_time) {
+              dominant_name_index = name_time_dict[k].size() - 1;
+            }
+
+            elem["tag"][0] = name_time_dict[k][dominant_name_index].first;
             elem["tag"][1] = combo_dict[k];
-            elem["tag"][2] = time_dict[k] - start_time;
+            elem["tag"][2] = name_time_dict[k][index == -1 ? 0 : index].second;
 
             if (exit_time_dict.find(k) != exit_time_dict.end()) {
-              elem["tag"][3] = exit_time_dict[k] - time_dict[k];
+              elem["tag"][3] = exit_time_dict[k] - name_time_dict[k][index == -1 ? 0 : index].second;
             } else {
               elem["tag"][3] = -1;
             }
@@ -348,7 +392,20 @@ namespace aperf {
               elem["parent"] = p;
             }
 
-            result_list.push_back(elem);
+            result_list.push_back(k);
+            result_map[k] = elem;
+          }
+
+          this->json_result[msg_key][0] = start_time;
+
+          for (auto &pair : result_map.items()) {
+            auto &elem = pair.value();
+            if (start_time >= elem["tag"][2]) {
+              elem["tag"][3] = (unsigned long long)elem["tag"][3] - (start_time - (unsigned long long)elem["tag"][2]);
+              elem["tag"][2] = 0;
+            } else {
+              elem["tag"][2] = (unsigned long long)elem["tag"][2] - start_time;
+            }
           }
         } else if (msg == "<SAMPLE>") {
           for (auto &elem : subprocesses) {
@@ -377,6 +434,7 @@ namespace aperf {
                 event_name = extra_event_name;
               }
 
+              pid_tid_result["first_time"] = first_sample_time_dict[elem.first + "_" + elem2.first];
               pid_tid_result[event_name] = nlohmann::json::array();
               pid_tid_result[event_name].push_back(res.output);
               pid_tid_result[event_name].push_back(res.output_time_ordered);
